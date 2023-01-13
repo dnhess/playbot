3a08ae4e582c8d4b46b91a5216de47dcbb046567
Dillon Hess <dnhess20@gmail.com>
2023-01-12 20:06:36 -0600

    feat: allow global commands

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN, BASE_API_URL, MONODB_URL } =
-  process.env;
+const {
+  CLIENT_ID,
+  GUILD_ID = '',
+  DISCORD_TOKEN,
+  BASE_API_URL,
+  MONODB_URL,
+} = process.env;
 
 // If any of the required environment variables are missing, throw an error
-if (!CLIENT_ID || !GUILD_ID || !DISCORD_TOKEN || !BASE_API_URL || !MONODB_URL) {
+if (!CLIENT_ID || !DISCORD_TOKEN || !BASE_API_URL || !MONODB_URL) {
   throw new Error('One or more environment variables are missing.');
 }
 
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -26,10 +26,17 @@ const rest = new REST({ version: '10' }).setToken(config.DISCORD_TOKEN);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     // TODO: Remove config.GUILD_ID to deploy global commands
-    await rest.put(
-      Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
-      { body: commands }
-    );
+    // If NODE_ENV is production, deploy global commands
+    if (process.env.NODE_ENV === 'production') {
+      await rest.put(Routes.applicationCommands(config.CLIENT_ID), {
+        body: commands,
+      });
+    } else {
+      await rest.put(
+        Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
+        { body: commands }
+      );
+    }
 
     console.log(
       `Successfully reloaded ${commands.length} application (/) commands.`
