86796a6fd504d4f43c77202bf474f92d15710cdf
Dillon Hess <dnhess20@gmail.com>
2023-04-13 18:57:58 -0500

    Tesseract memory improvements (hopefully)

--- src/messages/ocr.ts
+++ src/messages/ocr.ts
@@ -1,31 +1,39 @@
 import type { Message } from 'discord.js';
 import Tesseract from 'tesseract.js';
 
+const recognizeAndReply = (url: string, message: Message) => {
+  return Tesseract.recognize(url, 'eng', { cacheMethod: 'none' })
+    .then(({ data: { text } }) => {
+      if (text.includes('Prizes are not available')) {
+        message.reply(
+          `Beep boop! I noticed you uploaded an image with the text "Prizes are not available in your region." This typically means that you are in an unsupported country. The system can take time to detect your location, which might explain why you were able to participate in drops previously.`
+        );
+      }
+    })
+    .catch((error: any) => {
+      console.error(error);
+      console.log('Error while trying to OCR image.');
+    });
+};
+
 export const checkRegion = (message: Message) => {
-  message?.attachments?.forEach((attachment: { url: any }) => {
-    const { url } = attachment;
+  const tasks = message.attachments
+    .filter((attachment: { url: string }) => {
+      const { url } = attachment;
+      return (
+        url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg')
+      );
+    })
+    .map((attachment: { url: string }) => {
+      return recognizeAndReply(attachment.url, message);
+    });
 
-    // If the attachment is not an image, return
-    if (
-      !url.endsWith('.png') &&
-      !url.endsWith('.jpg') &&
-      !url.endsWith('.jpeg')
-    ) {
-      return;
-    }
-
-    Tesseract.recognize(url)
-      .then(({ data: { text } }) => {
-        // Reply to the message with the text if it contains the phrase "Prizes are not available"
-        if (text.includes('Prizes are not available')) {
-          message.reply(
-            `Beep boop! I noticed you uploaded an image with the text "Prizes are not available in your region." This typically means that you are in an unsupported country. The system can take time to detect your location, which might explain why you were able to participate in drops previously.`
-          );
-        }
-      })
-      .catch((error: any) => {
-        console.error(error);
-        console.log('Error while trying to OCR image.');
-      });
-  });
+  Promise.all(tasks)
+    .then(() => {
+      console.log('All OCR tasks completed.');
+    })
+    .catch((error: any) => {
+      console.error(error);
+      console.log('Error while processing OCR tasks.');
+    });
 };
