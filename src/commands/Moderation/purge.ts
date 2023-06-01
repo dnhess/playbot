791e9abf63b62416348a5115cfc44f5c91461b05
Dillon Hess <dnhess20@gmail.com>
2023-06-01 18:05:20 -0500

    adjust purge command

--- src/commands/Moderation/purge.ts
+++ src/commands/Moderation/purge.ts
@@ -17,7 +17,7 @@ export const data = new SlashCommandBuilder()
       .setMaxValue(100)
       .setRequired(true)
   )
-  .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages);
+  .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator);
 // eslint-disable-next-line consistent-return
 export const execute = async (interaction: CommandInteraction) => {
   const number = interaction.options.getInteger('amount');
@@ -27,45 +27,6 @@ export const execute = async (interaction: CommandInteraction) => {
     .setDescription(
       `:white_check_mark: Successfully **deleted** ${number} messages!`
     );
-
-  // Before bulk deleting the messages, ensure the user is not trying to delete a high role message
-  // This is done by fetching the messages and checking the author's highest role
-
-  // Fetch the messages
-  const messages = await interaction.channel?.messages.fetch({
-    limit: number,
-  });
-
-  // Check the author's highest role
-  const highestRole = interaction.member?.roles.highest;
-
-  // Filter the messages
-  const filteredMessages = messages?.filter((message) => {
-    const authorHighestRole = message.member?.roles.highest;
-
-    // If the author's highest role is higher than the user's highest role, return false
-    if (authorHighestRole?.position >= highestRole?.position) {
-      return false;
-    }
-
-    return true;
-
-    // If the author's highest role is lower than the user's highest role, return true
-  });
-
-  // If the filtered messages are less than the number of messages the user wants to delete, return an error
-
-  if (filteredMessages?.size !== 0) {
-    const filteredEmbedError = new EmbedBuilder()
-      .setColor('#7E47F3')
-      .setDescription(
-        `:x: You cannot delete messages that are from a higher role than you!`
-      );
-
-    return interaction.reply({ embeds: [filteredEmbedError], ephemeral: true });
-
-    // If the filtered messages are equal to the number of messages the user wants to delete, bulk delete the messages
-  }
 
   await interaction.channel?.bulkDelete(number);
 
