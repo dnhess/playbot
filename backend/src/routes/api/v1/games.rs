b7bc05afbc4eaeba1fe655926e562f91b986f3ac
Dillon Hess <dnhess20@gmail.com>
2024-02-24 15:56:42 -0600

    fix top api call

--- backend/src/routes/api/v1/games.rs
+++ backend/src/routes/api/v1/games.rs
@@ -1,7 +1,6 @@
 use crate::utils::{cache_in_redis, fetch_from_redis};
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
-use std::collections::HashSet;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct ApiResponse {
@@ -25,7 +24,8 @@ pub struct Payload {
 pub struct Game {
     pub id: String,
     pub name: Option<String>,
-    pub title: Option<String>,
+    #[serde(rename = "type")]
+    pub game_type: Option<String>,
     pub description: Option<String>,
     #[serde(rename = "posterImageUrl")]
     pub poster_image_url: Option<String>,
@@ -35,10 +35,6 @@ pub struct Game {
     pub app_url: Option<String>,
     #[serde(rename = "promoImageUrl")]
     pub promo_image_url: Option<String>,
-    #[serde(rename = "helpUrl")]
-    pub help_url: Option<String>,
-    #[serde(rename = "forceRemoteDistribution")]
-    pub force_remote_distribution: Option<bool>,
 }
 
 #[tracing::instrument(name = "Fetching all playbite games", skip(redis_pool))]
@@ -53,49 +49,44 @@ pub async fn get_all_games(redis_pool: actix_web::web::Data<deadpool_redis::Pool
 }
 
 async fn fetch_games_from_api() -> Result<Payload, String> {
-    let url = "https://playbiteapi.azurewebsites.net/api/feed?plat=web";
+    let url = "https://playbiteapi.azurewebsites.net/api/games";
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Starging request to API: {:?}", url);
+
     let response = match reqwest::get(url).await {
         Ok(res) => res,
         Err(err) => return Err(format!("Failed to make request to API: {:?}", err)),
     };
+    let status = response.status();
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Response status from API: {:?}", status);
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Response from API: {:?}", response);
 
-    let items: Vec<Item> = match response.json::<Vec<Item>>().await {
-        Ok(data) => data,
-        Err(err) => return Err(format!("Failed to parse Playbite response: {:?}", err)),
+    // Filter games where game_type is "App"
+    let games: Vec<Game> = match response.json().await {
+        Ok(games) => games,
+        Err(err) => return Err(format!("Failed to parse response from API: {:?}", err)),
     };
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Games found in API: {:?}", games);
 
-    let mut all_games = Vec::new();
-    let mut latest_releases = Vec::new();
-    let mut unique_ids = HashSet::new();
-
-    for item in items {
-        match item.title.as_str() {
-            "All Games" => all_games = item.payload.games,
-            "Latest Releases" => latest_releases = item.payload.games,
-            _ => {}
-        }
-    }
-
-    // Combine all_games and latest_releases into one Vec<Game>, removing duplicates
-    let mut combined_games = Vec::new();
-    for game in all_games.into_iter().chain(latest_releases.into_iter()) {
-        if unique_ids.insert(game.id.clone()) {
-            combined_games.push(game);
-        }
-    }
+    let filtered_games: Vec<Game> = games
+        .into_iter()
+        .filter(|game| game.game_type.as_deref() == Some("App"))
+        .collect();
 
     Ok(Payload {
-        games: combined_games,
+        games: filtered_games,
     })
 }
 
 pub(crate) async fn fetch_games(
     redis_pool: &web::Data<deadpool_redis::Pool>,
 ) -> Result<Payload, HttpResponse> {
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Setting up redis connection.");
     let mut connection = match redis_pool.get().await {
         Ok(conn) => conn,
         Err(_) => return Err(HttpResponse::InternalServerError().json("Redis connection error")),
     };
+
+    tracing::event!(target: "backend", tracing::Level::DEBUG, "Fetching games from Redis.");
 
     match fetch_from_redis::<Payload>("all_games", &mut connection).await {
         Ok(games) => Ok(games),
