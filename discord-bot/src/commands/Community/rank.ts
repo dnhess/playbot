8ef1975f1479280952aed7c2d9770c3941a83b3f
Dillon Hess <dnhess20@gmail.com>
2024-02-24 16:25:14 -0600

    small rank card style update

--- discord-bot/src/commands/Community/rank.ts
+++ discord-bot/src/commands/Community/rank.ts
@@ -71,7 +71,68 @@ export const execute = async (interaction: CommandInteraction) => {
     .setStatus('none')
     .setGraphemeProvider(BuiltInGraphemeProvider.FluentEmojiFlat)
     .setStyles({
-      container: 'text-2xl',
+      progressbar: {
+        thumb: {
+          style: {
+            backgroundColor: '#fff',
+          },
+        },
+        track: {
+          style: {
+            backgroundColor: '#6632ec',
+          },
+        },
+      },
+      statistics: {
+        level: {
+          text: {
+            style: {
+              color: '#fff',
+            },
+          },
+        },
+        xp: {
+          text: {
+            style: {
+              color: '#fff',
+            },
+          },
+        },
+        rank: {
+          text: {
+            style: {
+              color: '#fff',
+            },
+          },
+        },
+      },
+      container: {
+        style: {
+          backgroundColor: '#000',
+        },
+      },
+      overlay: {
+        style: {
+          backgroundColor: '#000',
+        },
+      },
+      background: {
+        style: {
+          backgroundColor: '#6632ec',
+        },
+      },
+      username: {
+        text: {
+          style: {
+            color: '#fff',
+          },
+        },
+        handle: {
+          style: {
+            color: '#fff',
+          },
+        },
+      },
     });
 
   const Card = await rank.build({
