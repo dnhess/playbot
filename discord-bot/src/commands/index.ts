7a7547e6850a5b4210447a796a7a56d5a4d27c4a
Dillon Hess <dnhess20@gmail.com>
2024-03-29 15:30:41 -0500

    remove report add cmd, update express

--- discord-bot/src/commands/index.ts
+++ discord-bot/src/commands/index.ts
@@ -26,6 +26,5 @@ export * as game from './Playbite/game';
 export * as managetopleaderboardreport from './Playbite/manage-top-leaderboard-report';
 export * as play from './Playbite/play';
 export * as referral from './Playbite/referral';
-export * as reportad from './Playbite/reportad';
 export * as top from './Playbite/top';
 export * as user from './Playbite/user';
