e0cfa6994cb96ecd4377c8118f36924348736090
Dillon Hess <dnhess20@gmail.com>
2024-08-12 09:20:24 -0500

    Remove top command

--- discord-bot/src/commands/index.ts
+++ discord-bot/src/commands/index.ts
@@ -27,5 +27,4 @@ export * as game from './Playbite/game';
 export * as managetopleaderboardreport from './Playbite/manage-top-leaderboard-report';
 export * as play from './Playbite/play';
 export * as referral from './Playbite/referral';
-export * as top from './Playbite/top';
 export * as user from './Playbite/user';
